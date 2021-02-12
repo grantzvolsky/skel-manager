d11a36065531596d5a8d7368eb79def8dae91780
Grant Zvolsky <grant@zvolsky.org>
2021-02-12 03:11:27 +0100

    dev-shell: pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
-with import <nixpkgs> {};
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/3590f02e7d5760e52072c1a729ee2250b5560746.tar.gz") {} }:
+with pkgs;
 let
   my_vim = callPackage ./nix/vim/vim.nix {};
 in pkgs.mkShell {
   name = "skm-shell";
-  nativeBuildInputs = [ git my_vim fzf tree shellcheck ];
+  nativeBuildInputs = [ my_vim git fzf tree shellcheck ];
   shellHook = ''
     set -o vi
     alias skm='$(realpath ./skel-manager.sh) '
