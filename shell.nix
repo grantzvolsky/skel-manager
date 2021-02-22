982ad775f6d1c12c6ddc96ae64f69853f1624155
Grant Zvolsky <grant@zvolsky.org>
2021-02-22 12:46:14 +0100

    dev-shell: add a basic nix derivation for skm

--- shell.nix
+++ shell.nix
@@ -1,12 +1,12 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/3590f02e7d5760e52072c1a729ee2250b5560746.tar.gz") {} }:
 with pkgs;
 let
+  skm = pkgs.writeShellScriptBin "skm" (builtins.readFile ./skel-manager.sh); # TODO an independent derivation
   my_vim = callPackage ./nix/vim/vim.nix {};
 in pkgs.mkShell {
   name = "skm-shell";
-  nativeBuildInputs = [ my_vim git fzf inotify-tools tree shellcheck ];
+  nativeBuildInputs = [ fzf git inotify-tools my_vim shellcheck skm tree ];
   shellHook = ''
     set -o vi
-    alias skm='$(realpath ./skel-manager.sh) '
   '';
 }
